5c37ccc7cbe3a4b7db06e433c4cff5659ce628c5
eshaeshita <esha.cse.buet@gmail.com>
2015-02-25 10:40:03 -0600

    time

--- gene_expr.cpp
+++ gene_expr.cpp
@@ -10,18 +10,23 @@
 #include "attributedata.h"
 #include "graphhelper.h"
 #include "calculator.h"
+#include "timer.h"
 
 using namespace std;
 
 
 int main()
 {
+    double start,end,elapsed;
     freopen("out.txt","w",stdout);
     GraphInputData graph = GraphInputData::getData("network.txt");
     AttributeData attribute = AttributeData::getAttributeData("attrs.txt");
-
-    Calculator calc(graph, attribute, 0.3, 100);
+    GET_TIME(start);
+    Calculator calc(graph, attribute, 0.3, 70);
     calc.calculate();
+    GET_TIME(end);
+    elapsed = end-start;
+    cout<<elapsed<<"\n";
     calc.printResult();
     return 0;
 }
