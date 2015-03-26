a9903a713baa283ef0b2e3b3f4dc76cd89a4bcf4
Dipranjan Das <dipranjan@eyeball.com>
2015-03-26 18:48:59 +0600

    added argc

--- gene_expr.cpp
+++ gene_expr.cpp
@@ -15,18 +15,26 @@
 using namespace std;
 
 
-int main()
+int main(int argc, char *argv[])
 {
     //freopen("out.txt","w",stdout);
 
     double start,end,elapsed;
+
+    double threshold = 0.3;
+    int minMatch = 70;
+    int thread = 5;
+
+    if (argc>1) threshold = stod(argv[1]);
+    if (argc>2) minMatch = stoi(argv[2]);
+    if (argc>3) thread = stoi(argv[3]);
 
     GraphInputData graph = GraphInputData::getData("network.txt");
     AttributeData attribute = AttributeData::getAttributeData("attrs.txt");
 
     GET_TIME(start);
 
-    Calculator calc(graph, attribute, 0.3, 100, 5);
+    Calculator calc(graph, attribute, threshold, minMatch, thread);
     calc.calculate();
 
     GET_TIME(end);
